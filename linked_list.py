757cf0d1a89e2934539d0dfa1ec2c9ed6c994d33
Casey MacPhee <caseymacphee@gmail.com>
2014-10-26 16:38:49 -0700

    Fixed the confusing logic in remove and implemented more thorough testing for the core features.

--- linked_list.py
+++ linked_list.py
@@ -1,22 +1,11 @@
 from node import Node
 
 class Linked_list(object):
-
-
-	def __init__(self, node = None):
-		
-		if type(node) != Node and node is not None:
-			raise TypeError('Object passed in must be of type Node')
-
-		self.front = node
-		if node is not None:
-			self.size = 1
-		else:
-			self.size = 0
-
+	def __init__(self):
+		self.front = None
+		self.currentsize = 0
 
 	def __str__(self):
-		
 		current = self.front
 		if self.front is None:
 			return "()"
@@ -31,13 +20,11 @@ class Linked_list(object):
 			string += ")"
 			return string
 
-
 	def __iter__(self):
 		self.current = self.front
 		return self
 
 	def next(self):
-
 		if self.current is None:
 			raise StopIteration
 		else:
@@ -46,7 +33,6 @@ class Linked_list(object):
 			return temp
 
 	def __add__(self, other):
-
 		i = self.__iter__()
 		j = other.__iter__()
 		newlist = Linked_list()
@@ -54,88 +40,70 @@ class Linked_list(object):
 			newlist.append(val)
 		for val in j:
 			newlist.append(val)
-		newlist.size = self.size + other.size
+		newlist.currentsize = self.currentsize + other.currentsize
 		return newlist
 
 	def append(self, val):
-
 		current = self.front
 		other = Node(val)
-		
+		self.currentsize += 1
 		if self.front is None:
 			self.front = other
-			self.size += 1
-
 		else:
 			while current.next is not None:
 				current = current.next
 			current.next = other
-			self.size += 1
-
 
 	def remove(self, value):
+		if self.front is not None:
+			current = self.front
+			if self.front.data == value:
+				self.front = self.front.next
+				self.currentsize -= 1
+			else:
+				while current.next is not None:
+					prev = current
+					current = current.next
+					if current.data == value:
+						prev.next = current.next
+						self.currentsize -=1
 
-		current = self.front
-		if self.front.data == value:
-			self.front = self.front.next
-		else:
-			while current.next.data != value and current.next is not None:
-				current = current.next
-			if current.next.data == value:
-				current.next = current.next.next
-				self.size -= 1
-
-	
 	def search(self, value):
-
-		current = self.front
-
-		if self.front.data == value:
-			return self.front
-		while current.next is not None and current.next.data != value:
-			current = current.next
-		if current.next is None:
+		if self.front is None:
 			return None
-		else:
-			return current.next
-
+		current = self.front
+		if current.data == value:
+			return current
+		while current.next is not None:
+			current = current.next
+			if current.data == value:
+				return current
+		return None
 
 	def insert(self, val):
-
 		node = Node(val)
 		node.next = self.front
 		self.front = node
-		self.size += 1
-
+		self.currentsize += 1
 
 	def pop(self):
-
+		if self.front is None:
+			return None
 		current = self.front.data
 		self.front = self.front.next
-		self.size -= 1
+		self.currentsize -= 1
 		return current
 
-
 	def size(self):
-
-		return self.size
-
+		return self.currentsize
 
 	def join(self, other):
-
 		if type(other) != Linked_list:
 			raise TypeError("Object passed in must be of type Linked_list")
-
 		current = self.front
-
 		while current.next is not None:
 			current = current.next
-
 		current.next = other.front
-		self.size += other.size
-
-
-
-
+		self.currentsize += other.currentsize
 
 
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,37 +1,57 @@
 import pytest
 from linked_list import *
 
-s = Linked_list()
+newlist = Linked_list()
 
 
 def test_insert():
-	
-	s.insert(1)
-	s.insert(2)
-	s.insert(3)
-	s.insert(4)
-	y = str(s)
-	assert y == '(4, 3, 2, 1)'
+	newlist.insert(1)
+	newlist.insert(2)
+	newlist.insert(3)
+	newlist.insert(4)
+	tostring = str(newlist)	
+	assert tostring == '(4, 3, 2, 1)'
 
 def test_pop():
-	x = s.pop()
-	assert x == 4
+	lastin = newlist.pop()
+	assert lastin == 4
+	lastin = newlist.pop()
+	assert lastin == 3
+	newlist.pop()
+	newlist.pop()
+	assert newlist.pop() == None
+	assert newlist.pop() == None
 
 def test_size():
-	assert s.size == 3
+	newlist.insert(1)
+	newlist.insert(2)
+	newlist.insert(6)
+	assert newlist.size() == 3
+	testlist = Linked_list()
+	assert testlist.size() == 0
 
 def test_search():
-	j = s.search(30)
-	assert j is None
-
-	i = s.search(2)
-	assert i.data == 2
+	foundval = newlist.search(30)
+	assert foundval is None
+	foundval = newlist.search(2)
+	assert foundval.data == 2
+	testlist = Linked_list()
+	foundval = testlist.search(1)
+	assert foundval == None
+	testlist.insert(5)
+	testlist.insert(None)
+	foundval = testlist.search(None)
+	assert foundval.data == None
 
 def test_remove():
-	s.insert(4)
-	s.insert(5)
-	s.remove(1)
-	s.remove(3)
-	s.remove(5)
-	assert str(s) == '(4, 2)'
+	newlist.insert(4)
+	newlist.insert(5)
+	newlist.remove(1)
+	newlist.remove(3)
+	newlist.remove(5)
+	newlist.remove(6)
+	assert str(newlist) == '(4, 2)'
+	newlist.remove(4)
+	newlist.remove(2)
+	assert str(newlist) == '()'
 
